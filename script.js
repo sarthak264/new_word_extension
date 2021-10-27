c3c876b46482bacdfc657ced285b494a46337007
sarthak264 <sarthaksharm372@gmail.com>
2021-10-27 18:34:36 +0530

    added a basic toggle for switching theme

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const word = document.querySelector(".word");
 const meaning = document.querySelector(".meaning");
+const section = document.querySelector("section");
+const checkbox = document.getElementById("theme_input");
 
 async function getWord() {
   const response = await fetch("https://random-words-api.vercel.app/word");
@@ -9,4 +11,14 @@ async function getWord() {
   word.textContent = data.word;
   meaning.textContent = data.definition;
 }
+
+function changeTheme() {
+  if (checkbox.checked) {
+    section.classList.add("dark");
+  } else {
+    section.classList.remove("dark");
+  }
+}
+
 getWord();
+checkbox.addEventListener("click", changeTheme);
